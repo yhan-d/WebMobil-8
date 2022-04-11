6e458a2a00f1cd6a8e7b0d59e59f5b52046750fb
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-11 14:13:36 +0300

    Register process

---
+++ Identity101/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Identity101.Models.Identity;
+using Identity101.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity101.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.AddModelError(string.Empty, "Bir hata oluştu");
+                return View(model);
+            }
+            
+            var user = new ApplicationUser { 
+                UserName = model.UserName, 
+                Email = model.Email,
+                Name = model.Name,
+                Surname = model.Surname
+            };
+            
+            var result = await _userManager.CreateAsync(user,model.Password);
+            if (result.Succeeded)
+            {
+                //TODO: Email gönderme - Aktivasyon
+                //TODO: Login olma
+                return RedirectToAction("Login");
+            }
+            var messages = string.Join("\n", result.Errors.Select(x => x.Description));
+            ModelState.AddModelError(string.Empty, messages);
+
+            return View(model);
+        }
+    }
+}
--- Identity101/Program.cs
+++ Identity101/Program.cs
@@ -1,9 +1,44 @@
 using Identity101.Data;
+using Identity101.Models.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var con1 = builder.Configuration.GetConnectionString("con1");
 builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(con1));
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.AllowedForNewUsers = false;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+    options.User.RequireUniqueEmail = true;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+
 builder.Services.AddControllersWithViews();
 
 
@@ -21,6 +56,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
