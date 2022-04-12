af57541a73c8c330be58eae2063904258186a390
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-12 14:38:07 +0300

    Logout Process

--- Identity101/Controllers/AccountController.cs
+++ Identity101/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Identity101.Models.Identity;
 using Identity101.Models.Role;
 using Identity101.Services.Email;
 using Identity101.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
@@ -151,6 +152,12 @@ namespace Identity101.Controllers
            
         }
         
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+        }
 
     }
 }
