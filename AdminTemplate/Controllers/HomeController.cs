4022e9a47931513cdb1ff311dde100045903a9ec
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-19 10:48:31 +0300

    First Step of Admin Template

---
+++ AdminTemplate/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
