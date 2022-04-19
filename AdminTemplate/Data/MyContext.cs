88bdec48b6f8ef9b4f93854f73ce0215d979d086
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-19 15:00:11 +0300

    Add Migration

--- AdminTemplate/Data/MyContext.cs
+++ AdminTemplate/Data/MyContext.cs
@@ -3,54 +3,52 @@ using AdminTemplate.Models.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace AdminTemplate.Data
+namespace AdminTemplate.Data;
+
+public sealed class MyContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
 {
-    public sealed class MyContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    public MyContext(DbContextOptions options)
+        : base(options)
     {
-        public MyContext(DbContextOptions options) : base(options)
-        {
-
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<ApplicationUser>(entity =>
-            {
-                entity.Property(x => x.Name).HasMaxLength(50).IsRequired(false);
-                entity.Property(x => x.Surname).HasMaxLength(50).IsRequired(false);
-                entity.Property(x => x.RegisterDate).HasColumnType("datetime");
-            });
-
-            builder.Entity<ApplicationRole>(entity =>
-            {
-                entity.Property(x => x.Description).HasMaxLength(120).IsRequired(false);
-            });
-
-            builder.Entity<Category>(entity =>
-            {
-                entity.HasIndex(x => x.Id);
-                entity.HasMany(x => x.Products)
-                    .WithOne(x => x.Category)
-                    .HasForeignKey(x => x.Category.Id);
-                entity.Property(x => x.Name).IsRequired().HasMaxLength(50);
-                entity.Property(x => x.Description).IsRequired(false).HasMaxLength(250);
-            });
-
-            builder.Entity<Product>(entity =>
-            {
-                entity.HasIndex(x => x.Id);
-                entity.Property(x => x.Id).HasDefaultValue(Guid.NewGuid());
-                entity.HasOne(x => x.Category)
-                     .WithMany(x => x.Products)
-                     .HasForeignKey(x => x.CategoryId);
-                entity.Property(x => x.Name).IsRequired().HasMaxLength(50);
-                entity.Property(x => x.UnitPrice).HasPrecision(8, 2);
-
-            });
-        }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        //Fluent API
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.Property(x => x.Name).HasMaxLength(50).IsRequired(false);
+            entity.Property(x => x.Surname).HasMaxLength(50).IsRequired(false);
+            entity.Property(x => x.RegisterDate).HasColumnType("datetime");
+        });
+
+        builder.Entity<ApplicationRole>(entity =>
+        {
+            entity.Property(x => x.Description).HasMaxLength(120).IsRequired(false);
+        });
+
+        builder.Entity<Category>(entity =>
+        {
+            entity.HasIndex(x => x.Id);
+            entity.HasMany(x => x.Products)
+                .WithOne(x => x.Category)
+                .HasForeignKey(x => x.CategoryId);
+            entity.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            entity.Property(x => x.Description).IsRequired(false).HasMaxLength(250);
+        });
+
+        builder.Entity<Product>(entity =>
+        {
+            entity.HasIndex(x => x.Id);
+            entity.Property(x => x.Id).HasDefaultValue(Guid.NewGuid());
+            entity.HasOne(x => x.Category)
+                .WithMany(x => x.Products)
+                .HasForeignKey(x => x.CategoryId);
+            entity.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            entity.Property(x => x.UnitPrice).HasPrecision(8, 2);
+        });
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
 }
--- AdminTemplate/Program.cs
+++ AdminTemplate/Program.cs
@@ -1,6 +1,7 @@
 using AdminTemplate.Data;
 using AdminTemplate.Models.Identity;
 using AdminTemplate.Services.Email;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
