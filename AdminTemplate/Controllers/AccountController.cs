7a5503fdce9b72ca0f2ec9aa29955a915af99b8b
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-20 09:55:55 +0300

    Register Page

---
+++ AdminTemplate/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
