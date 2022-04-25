46207d72c1413338f1e191224d565d9e21cc5a26
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-25 09:51:48 +0300

    report 1

---
+++ AdminTemplate/ViewModels/Dashboard/ProductReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminTemplate.ViewModels.Dashboard
+{
+    public class ProductReportViewModel
+    {
+        [Display(Name="Adet")]
+        public int Count { get; set; }
+        [Display(Name="Toplam")]
+        public decimal Total { get; set; }
+            
+    
+    }
+}
