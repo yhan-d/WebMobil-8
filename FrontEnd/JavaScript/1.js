fb9eb40eab0e529ace4e2d99dd7fdfe201b7be4e
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-25 15:00:16 +0300

    innerHTML

--- FrontEnd/JavaScript/1.js
+++ FrontEnd/JavaScript/1.js
@@ -1,3 +1,26 @@
 function mesaj(){
-    alert("Merhaba JavaScript");
+   // alert("Merhaba JavaScript");
+   var pElement = document.getElementById("icerik");
+   pElement.innerHTML= "<strong>Merhaba JavaScript</strong>" ;
 }
+
+//değişken tanımlama
+var degisken1 = "Ahmet";
+degisken1 = 123;
+degisken1 = 1.4;
+degisken1 = true;
+
+degisken1 = [1,2,3];
+degisken1[3] = "Kamil";
+degisken1[10] = "Kamil";
+console.log(degisken1);
+
+degisken1 = {
+    ad: "Kamil",
+    soyad: "Fıdıl",
+    yas: 23,
+    meslek: "Çalışan",
+    bilgileriGoster : function(){
+        return this.ad + " " + this.soyad + " " + this.yas + " " + this.meslek;
+    },
+};
