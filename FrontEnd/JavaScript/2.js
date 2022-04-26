517bfc6f99fa3ee47bad6655378f879c244f4749
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-26 14:00:25 +0300

    JavaScript Exercise 2

--- FrontEnd/JavaScript/2.js
+++ FrontEnd/JavaScript/2.js
@@ -56,3 +56,22 @@ var diziDongu2 = () =>{
 
 }
 
+var a = [1, 2, 3, 4, 5, 6, 6, 8, 9, 10, 2, 2]; //return 6
+var b = [2, 4, 5, 3, 1, 1, 3, 4, 5, 5]; //retrun 1
+
+function checkRepeat(array) {
+  var distinct = [];
+  for (var i = 1; i < array.length; i++) {
+      var item1 = array[i-1];
+      var item2 = array[i];
+      if (item1 == item2 && distinct.indexOf(item1) == -1) {
+          distinct.push(item1);
+      }
+  }
+  return distinct.toString();
+}
+
+console.log(checkRepeat(a));
+console.log(checkRepeat(b));
+
+
