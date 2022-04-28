43d94c0df6ca9736071138b4103b84cd2801d5fd
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-04-28 10:51:49 +0300

    Map JavaScript API

--- FrontEnd/JavaScript/4.js
+++ FrontEnd/JavaScript/4.js
@@ -1,6 +1,6 @@
 console.log("4.js");
 
-var getLocation = () =>{
+var initMap = () =>{
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition);
     }
@@ -11,4 +11,38 @@ var getLocation = () =>{
 
 var showPosition = (position) => {
     console.log(position);
+    var pos ={lat:position.coords.latitude,
+              lng: position.coords.longitude
+    };
+    console.log(pos);
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv,{
+        center:pos,
+        zoom:18,
+        mapTypeId: "terrain",
+    });
+
+    var marker = new google.maps.Marker({
+        position:pos,
+        map:map
+    });
+
+    map.addListener('click', (e) =>{
+        console.log(e);
+        var posClick = {
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+        };
+        //markerları temizle
+        marker.setMap(null);
+        //tıklanan konuma göre marker ekle
+        marker = new google.maps.Marker({
+            position:posClick,
+            map:map,
+            title: 'Clicked location',
+            animation: google.maps.Animation.DROP
+        });
+    });
+
+  
 }
